bdff618ea36ecf96bbe29c178d9c543f968323cf
S4nfs <zyzzaestheticsgods@gmail.com>
2022-08-26 23:41:35 +0530

    event on connect and disconnect

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const path = require('path');
 const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
-
 const server = app.listen(port, () => console.log(`Listening iChat on port ${port}`))
 
 let socketsConnected = new Set();
@@ -12,40 +11,39 @@ let socketsConnected = new Set();
 const io = require('socket.io')(server)
 io.on('connection', onConnected)
 
-let people;
-let arr = [];
+var lobby = {};
+
 function onConnected(socket) {
 
-    //joined
+    //connect
     socket.on('joined', (joined) => {
         socketsConnected.add(socket.id)
-        people = {
-            id:socket.id,
-            username:joined.name    
-        }
-        //to emit an event to client side when the socket is connected
+        lobby[socket.id] = joined.name;
+        //io.emit will send to all the clients
+        //socket.broadcast.emit will send the message to all the other clients except the newly created connection
         io.emit('clients-total', socketsConnected.size)
-        //  people[socket.id] = joined.name;
-        arr.push(people);
+        socket.broadcast.emit('user-connect', joined.name)
+        io.emit("lobby-users", lobby)
     })
-    io.emit('yesJoined', arr);
-    
 
+    //disconnect
     socket.on('disconnect', () => {
-        io.emit('yesDisconnect', socket.id);
-        delete arr[socket.id];
+        socket.broadcast.emit('user-disconnect', user = lobby[socket.id]);
+        delete lobby[socket.id];
         socketsConnected.delete(socket.id)
         io.emit('clients-total', socketsConnected.size);
+        io.emit("lobby-users", lobby)
     });
 
     //handling hellomessage event from client side
-    socket.on('sendMessageToAll', (data) =>{
-        socket.broadcast.emit('toALLThePeopleConnectedInTheLobby', data) //If you want to broadcast to everyone except the person who connected you can use socket.broadcast.emit()
+    socket.on('sendMessageToAll', (data) => {
+        socket.broadcast.emit('toALLThePeopleConnectedInTheLobby', data)
     })
-    console.log(arr);
 
 
     socket.on('someoneIsTyping', (data) => {
         socket.broadcast.emit('feedback', data)
     })
+    console.log(lobby);
+
 }
